e8212219e0989bfc71702becaca023d55755bb5c
Felipe César <www.felipecesargomes@gmail.com>
2020-07-10 22:27:23 -0300

    Configuração para deploy no heroku

--- src/main/java/br/com/acme/domain/Aviso.java
+++ src/main/java/br/com/acme/domain/Aviso.java
@@ -20,20 +20,22 @@ import lombok.Setter;
  * @author carlosfilho
  *
  */
+
 @Entity
+@EqualsAndHashCode
 @Getter
 @Setter
-@EqualsAndHashCode
 @Table(name = "tb_avisos")
 public class Aviso implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String descricaoAviso;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "id_condominio")
 	private Condominio condominoAvisos;
